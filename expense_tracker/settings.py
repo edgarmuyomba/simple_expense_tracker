be0fb6b95ce459afa5077a0f2ff2357d81a12a86
edgarmuyomba <edgarmatthew247@gmail.com>
2023-11-18 13:27:33 +0300

    environment variables

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s7c4y_qxi-k^1-k_k_*tgyn3h+4afyx@+99w@&5#d=ag^gp79c'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,6 +35,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # my apps
+    'core',
+    'users',
+    # defualt apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
